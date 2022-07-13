0dfa9198765576974101bc870020dbf543327b40
Luis Viera <levieraf@gmail.com>
2022-07-12 21:07:07 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    Text,
+    View,
+} from 'react-native';
+
+const App = () => {
+    return (
+        <View style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            flex: 1
+        }}>
+            <Text>Token...</Text>
+        </View>
+    );
+};
+
+export default App;
