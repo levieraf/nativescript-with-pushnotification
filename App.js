902c8ede41246c3deebb86d8f0a629aa8560e4e9
Luis Viera <levieraf@gmail.com>
2022-07-13 22:06:18 -0500

    token

--- App.js
+++ App.js
@@ -1,72 +1,128 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     Text,
     View,
+    ToastAndroid,
+    Pressable,
+    StyleSheet
 } from 'react-native';
 // import PushNotificationIOS from "@react-native-community/push-notification-ios";
 import PushNotification from "react-native-push-notification";
 
-// Must be outside of any component LifeCycle (such as `componentDidMount`).
-PushNotification.configure({
-    // (optional) Called when Token is generated (iOS and Android)
-    onRegister: function (token) {
-      console.log("TOKEN:", token);
-    },
-  
-    // (required) Called when a remote is received or opened, or local notification is opened
-    onNotification: function (notification) {
-      console.log("NOTIFICATION:", notification);
-  
-      // process the notification
-  
-      // (required) Called when a remote is received or opened, or local notification is opened
-    //   notification.finish(PushNotificationIOS.FetchResult.NoData);
-    },
-  
-    // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
-    onAction: function (notification) {
-      console.log("ACTION:", notification.action);
-      console.log("NOTIFICATION:", notification);
-  
-      // process the action
-    },
-  
-    // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
-    onRegistrationError: function(err) {
-      console.error(err.message, err);
-    },
-  
-    // IOS ONLY (optional): default: all - Permissions to register.
-    permissions: {
-      alert: true,
-      badge: true,
-      sound: true,
-    },
-  
-    // Should the initial notification be popped automatically
-    // default: true
-    popInitialNotification: true,
-  
-    /**
-     * (optional) default: true
-     * - Specified if permissions (ios) and token (android and ios) will requested or not,
-     * - if not, you must call PushNotificationsHandler.requestPermissions() later
-     * - if you are not using remote notification or do not have Firebase installed, use this:
-     *     requestPermissions: Platform.OS === 'ios'
-     */
-    requestPermissions: true,
-  });
 
 const App = () => {
+    const [token, setToken] = useState('');
+    useEffect(() => {
+        // Must be outside of any component LifeCycle (such as `componentDidMount`).
+        PushNotification.configure({
+            // (optional) Called when Token is generated (iOS and Android)
+            onRegister: function (token) {
+                console.log("TOKEN:", token);
+                setToken(token.token);
+            },
+
+            // (required) Called when a remote is received or opened, or local notification is opened
+            onNotification: function (notification) {
+                console.log("NOTIFICATION:", notification);
+
+                // process the notification
+
+                // (required) Called when a remote is received or opened, or local notification is opened
+                //   notification.finish(PushNotificationIOS.FetchResult.NoData);
+            },
+
+            // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+            onAction: function (notification) {
+                console.log("ACTION:", notification.action);
+                console.log("NOTIFICATION:", notification);
+
+                // process the action
+            },
+
+            // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+            onRegistrationError: function (err) {
+                console.error(err.message, err);
+            },
+
+            // IOS ONLY (optional): default: all - Permissions to register.
+            permissions: {
+                alert: true,
+                badge: true,
+                sound: true,
+            },
+
+            // Should the initial notification be popped automatically
+            // default: true
+            popInitialNotification: true,
+
+            /**
+             * (optional) default: true
+             * - Specified if permissions (ios) and token (android and ios) will requested or not,
+             * - if not, you must call PushNotificationsHandler.requestPermissions() later
+             * - if you are not using remote notification or do not have Firebase installed, use this:
+             *     requestPermissions: Platform.OS === 'ios'
+             */
+            requestPermissions: true,
+        });
+
+        PushNotification.popInitialNotification((notification) => {
+            console.info('notification', notification);
+        });
+    }, []);
+
+
+    const sendTokenToServer = () => {
+        ToastAndroid.show("Token enviado correctamente!", ToastAndroid.SHORT);
+    };
+
     return (
         <View style={{
             alignItems: 'center',
             justifyContent: 'center',
+            padding: 15,
             flex: 1
         }}>
-            <Text>token</Text>
+            <Text>{token}</Text>
+
+            <Pressable
+                onPress={sendTokenToServer}
+                style={({ pressed }) => [
+                    {
+                        backgroundColor: pressed
+                            ? 'rgb(210, 230, 255)'
+                            : 'white'
+                    },
+                    styles.wrapperCustom
+                ]}>
+                {({ pressed }) => (
+                    <Text style={styles.text}>
+                        {pressed ? 'Pressed!' : 'Enviar Token'}
+                    </Text>
+                )}
+            </Pressable>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+    },
+    text: {
+        fontSize: 16
+    },
+    wrapperCustom: {
+        borderRadius: 8,
+        padding: 6
+    },
+    logBox: {
+        padding: 20,
+        margin: 10,
+        borderWidth: StyleSheet.hairlineWidth,
+        borderColor: '#f0f0f0',
+        backgroundColor: '#f9f9f9'
+    }
+});
 
 export default App;
